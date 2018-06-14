6d7313194f245504d79e6fd44754deefbef09537
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-06-14 23:20:50 +0200

    fix many sonar issues

--- WebServices/WsHTTP.php
+++ WebServices/WsHTTP.php
@@ -16,6 +16,9 @@ use Mactronique\TestWs\Factory\ResultFactory;
 
 class WsHTTP implements TestWebServicesInterface
 {
+    const CONFIG_KEY_ENV = 'env';
+    const CONFIG_KEY_DATAS = 'datas';
+    const CONFIG_KEY_RESPONSE = 'response';
     /**
      * @var array
      */
@@ -35,15 +38,15 @@ class WsHTTP implements TestWebServicesInterface
     public function __construct(array $config, ResultFactory $factory)
     {
         $this->config = $config;
-        if (!array_key_exists('env', $this->config) || !is_array($this->config['env']) || 0 == count($this->config['env'])) {
+        if (!array_key_exists(self::CONFIG_KEY_ENV, $this->config) || !is_array($this->config[self::CONFIG_KEY_ENV]) || 0 == count($this->config[self::CONFIG_KEY_ENV])) {
             throw new WebServiceException('Les environnements ne sont pas définis', 1);
         }
 
-        if (!array_key_exists('datas', $this->config) || !is_array($this->config['datas']) || 0 == count($this->config['datas'])) {
+        if (!array_key_exists(self::CONFIG_KEY_DATAS, $this->config) || !is_array($this->config[self::CONFIG_KEY_DATAS]) || 0 == count($this->config[self::CONFIG_KEY_DATAS])) {
             throw new WebServiceException('Les données ne sont pas définies', 1);
         }
 
-        if (!array_key_exists('response', $this->config) || !is_array($this->config['response']) || 0 == count($this->config['response'])) {
+        if (!array_key_exists(self::CONFIG_KEY_RESPONSE, $this->config) || !is_array($this->config[self::CONFIG_KEY_RESPONSE]) || 0 == count($this->config[self::CONFIG_KEY_RESPONSE])) {
             throw new WebServiceException('Les données de la réponse ne sont pas définies', 1);
         }
         $this->factory = $factory;
@@ -61,8 +64,8 @@ class WsHTTP implements TestWebServicesInterface
         $client = new Client(['http_errors' => false, 'timeout' => 12.0]);
         $output->writeln("Start : ".date('c'));
         $env = $this->config['env'];
-        $dataRequest = $this->config['datas'];
-        $responseData = $this->config['response'];
+        $dataRequest = $this->config[self::CONFIG_KEY_DATAS];
+        $responseData = $this->config[self::CONFIG_KEY_RESPONSE];
         $promises = (function () use ($env, $dataRequest, &$statsAll, $client, $responseData, &$statsData, $factory) {
             foreach ($env as $keyEnv => $url) {
                 $statsAll[$url] = ['started_at'=>microtime(true)];
@@ -79,7 +82,7 @@ class WsHTTP implements TestWebServicesInterface
             }
         })();
 
-        $responseData = $this->config['response'];
+        $responseData = $this->config[self::CONFIG_KEY_RESPONSE];
         (new \GuzzleHttp\Promise\EachPromise($promises, [
             'concurrency' => 10
         ]))->promise()->wait();

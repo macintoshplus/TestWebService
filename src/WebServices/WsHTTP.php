d85ede224f1a57ca83076b7660e70e72e8b1da44
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-10-01 21:02:01 +0200

    move code source

--- src/WebServices/WsHTTP.php
+++ src/WebServices/WsHTTP.php
@@ -7,6 +7,7 @@
  * @copyright 2016 - Jean-Baptiste Nahan
  * @license MIT
  */
+
 namespace Mactronique\TestWs\WebServices;
 
 use Symfony\Component\Console\Output\OutputInterface;
@@ -38,15 +39,27 @@ class WsHTTP implements TestWebServicesInterface
     public function __construct(array $config, ResultFactory $factory)
     {
         $this->config = $config;
-        if (!array_key_exists(self::CONFIG_KEY_ENV, $this->config) || !is_array($this->config[self::CONFIG_KEY_ENV]) || 0 == count($this->config[self::CONFIG_KEY_ENV])) {
+        if (
+            !array_key_exists(self::CONFIG_KEY_ENV, $this->config) ||
+            !is_array($this->config[self::CONFIG_KEY_ENV]) ||
+            0 == count($this->config[self::CONFIG_KEY_ENV])
+        ) {
             throw new WebServiceException('Les environnements ne sont pas définis', 1);
         }
 
-        if (!array_key_exists(self::CONFIG_KEY_DATAS, $this->config) || !is_array($this->config[self::CONFIG_KEY_DATAS]) || 0 == count($this->config[self::CONFIG_KEY_DATAS])) {
+        if (
+            !array_key_exists(self::CONFIG_KEY_DATAS, $this->config) ||
+            !is_array($this->config[self::CONFIG_KEY_DATAS]) ||
+            0 == count($this->config[self::CONFIG_KEY_DATAS])
+        ) {
             throw new WebServiceException('Les données ne sont pas définies', 1);
         }
 
-        if (!array_key_exists(self::CONFIG_KEY_RESPONSE, $this->config) || !is_array($this->config[self::CONFIG_KEY_RESPONSE]) || 0 == count($this->config[self::CONFIG_KEY_RESPONSE])) {
+        if (
+            !array_key_exists(self::CONFIG_KEY_RESPONSE, $this->config) ||
+            !is_array($this->config[self::CONFIG_KEY_RESPONSE]) ||
+            0 == count($this->config[self::CONFIG_KEY_RESPONSE])
+        ) {
             throw new WebServiceException('Les données de la réponse ne sont pas définies', 1);
         }
         $this->factory = $factory;
@@ -62,13 +75,21 @@ class WsHTTP implements TestWebServicesInterface
         $statsData = [];
         $resultFactory = $this->factory;
         $client = new Client(['http_errors' => false, 'timeout' => 12.0]);
-        $output->writeln("Start : ".date('c'));
+        $output->writeln("Start : " . date('c'));
         $env = $this->config['env'];
         $dataRequest = $this->config[self::CONFIG_KEY_DATAS];
         $responseData = $this->config[self::CONFIG_KEY_RESPONSE];
-        $promises = (function () use ($env, $dataRequest, &$statsAll, $client, $responseData, &$statsData, $resultFactory) {
+        $promises = (function () use (
+            $env,
+            $dataRequest,
+            &$statsAll,
+            $client,
+            $responseData,
+            &$statsData,
+            $resultFactory
+        ) {
             foreach ($env as $keyEnv => $url) {
-                $statsAll[$url] = ['started_at'=>microtime(true)];
+                $statsAll[$url] = ['started_at' => microtime(true)];
                 $headers = [];
                 if (isset($dataRequest['mime'])) {
                     $headers['Content-Type'] = $dataRequest['mime'];
@@ -77,16 +98,29 @@ class WsHTTP implements TestWebServicesInterface
                     $headers['Authorization'] = $dataRequest['authorization'];
                 }
 
-                yield $client->requestAsync($dataRequest['method'], $url, ['headers' => $headers, 'body' => $dataRequest['datas'], 'on_stats' => function (\GuzzleHttp\TransferStats $stats) use (&$statsAll, $url, $responseData, &$statsData, $resultFactory, $keyEnv) {
+                yield $client->requestAsync($dataRequest['method'], $url, [
+                    'headers' => $headers,
+                    'body' => $dataRequest['datas'],
+                    'on_stats' => function (\GuzzleHttp\TransferStats $stats) use (
+                        &$statsAll,
+                        $url,
+                        $responseData,
+                        &
+                        $statsData,
+                        $resultFactory,
+                        $keyEnv
+                    ) {
 
-                    if (isset($statsAll[$url]) && is_array($statsAll[$url])) {
-                        $statsArray = array_merge($statsAll[$url], $stats->getHandlerStats());
-                    } else {
-                        $statsArray = $stats->getHandlerStats();
-                    }
-                    $statsAll[$url] = $statsArray;
-                    $statsData[$url] = $resultFactory->makeResult($statsArray, $responseData, $keyEnv, $stats->getResponse());
-                },]);
+                        if (isset($statsAll[$url]) && is_array($statsAll[$url])) {
+                            $statsArray = array_merge($statsAll[$url], $stats->getHandlerStats());
+                        } else {
+                            $statsArray = $stats->getHandlerStats();
+                        }
+                        $statsAll[$url] = $statsArray;
+                        $statsData[$url] = $resultFactory->makeResult($statsArray, $responseData, $keyEnv,
+                            $stats->getResponse());
+                    },
+                ]);
             }
         })();
 
